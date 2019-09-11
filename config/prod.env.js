522b925126140fdcd3194fa610dc60f594f16619
wuzhujiang <2738442573@qq.com>
2019-09-11 17:15:03 +0800

    0

---
+++ config/prod.env.js
@@ -0,0 +1,7 @@
+module.exports = {
+    NODE_ENV: '"production"',
+    ENV_CONFIG: '"prod"',
+    // BASE_API: '"http://localhost:9093/"'
+    BASE_API: '"http://www.fancystore.cn/"'
+    // BASE_API: '"http://172.100.100.36:9093/"'
+}
